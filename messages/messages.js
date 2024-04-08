da71be2b0b634136a8dc440fde03f8095cb6c092
MaddoxRyan-FS <RAMaddox@student.fullsail.edu>
2024-04-07 22:02:20 -0500

    add unique response messages

---
+++ messages/messages.js
@@ -0,0 +1,29 @@
+module.exports = {
+    // GET
+    song_collection: "Song Collection",
+    song_collection_empty: `You have no songs in your collection. Add a song to see it in your collection`,
+    artists: "Artists",
+    artists_empty: `You have not added any artists to your collection. Add an artists to see it in your collection.`,
+    song_selected: "Song Selected",
+    artist_found: "Artist Found",
+  
+    // POST
+    song_submitted: "Song Submitted",
+    artist_submitted: "Artist Info Submitted",
+    song_post_duplicate: `This song info is already in the collection.`,
+    artist_post_duplicate: `This artist's info is already in the collection.`,
+    song_post_error: `Unable to save song info. Please try again.`,
+    artist_post_error: `Unable to save artist info. Please try again`,
+  
+    // PATCH
+    song_updated: "Song Updated",
+    artist_updated: "Artist Info Updated",
+  
+    // DELETE
+    song_deleted: "Song Deleted",
+    artist_deleted: "Artist Info Deleted",
+  
+    // 404
+    song_not_found: `Song could not be found. Please check the ID is correct.`,
+    artist_not_found: `Artist could not be found. Please check the ID is correct.`,
+  };
