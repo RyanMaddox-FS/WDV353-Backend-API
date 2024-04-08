8db104923f581e38da69a99828568f31f7ee8b4d
MaddoxRyan-FS <RAMaddox@student.fullsail.edu>
2024-04-07 22:03:19 -0500

    add validation

--- api/models/song.js
+++ api/models/song.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const songSchema = new Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  title: String ,
-  artist: String ,
-  album: String ,
+  title: { type: String, required: true },
+  artist: { type: String, required: true },
+  album: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Song', songSchema);
+module.exports = mongoose.model("Song", songSchema);
