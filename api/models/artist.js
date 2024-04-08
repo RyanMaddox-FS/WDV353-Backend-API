b4f29f8b476c6bbb368217d601807fa086db1a6a
MaddoxRyan-FS <RAMaddox@student.fullsail.edu>
2024-04-07 22:04:03 -0500

    add validation and song schema

--- api/models/artist.js
+++ api/models/artist.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const artistSchema = new Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: String ,
-  
-    albumName: String ,
-    albumYear: Number ,
-    ep:  Boolean ,
-
+  name: { type: String, required: true },
+  albumName: { type: String, required: true },
+  albumYear: { type: Number, required: true },
+  ep: { type: Boolean, required: true },
+  song: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Song",
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Artist', artistSchema);
+module.exports = mongoose.model("Artist", artistSchema);
